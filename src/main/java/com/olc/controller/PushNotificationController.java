761934fd1a211118e76e42aaa141713cf524b099
sps-tech <Thankyou@god1>
2020-09-15 10:29:26 +0530

    initial commit

---
+++ src/main/java/com/olc/controller/PushNotificationController.java
@@ -0,0 +1,41 @@
+package com.olc.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.olc.model.PushNotificationRequest;
+import com.olc.model.PushNotificationResponse;
+import com.olc.service.PushNotificationService;
+
+@RestController
+public class PushNotificationController {
+
+    private PushNotificationService pushNotificationService;
+
+    public PushNotificationController(PushNotificationService pushNotificationService) {
+        this.pushNotificationService = pushNotificationService;
+    }
+
+    @PostMapping("/notification/topic")
+    public ResponseEntity sendNotification(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotificationWithoutData(request);
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(),
+                "Notification has been sent."), HttpStatus.OK);
+    }
+
+    @PostMapping("/notification/token")
+    public ResponseEntity sendTokenNotification(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotificationToToken(request);
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(),
+                "Notification has been sent."), HttpStatus.OK);
+    }
+
+    @PostMapping("/notification/data")
+    public ResponseEntity sendDataNotification(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotification(request);
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(),
+                "Notification has been sent."), HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/olc/service/NotificationParameter.java
@@ -0,0 +1,15 @@
+package com.olc.service;
+public enum NotificationParameter {
+    SOUND("default"),
+    COLOR("#FFFF00");
+
+    private String value;
+
+    NotificationParameter(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+}
